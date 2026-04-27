b25d3325faab5c92e5ad65d1b02543caaa39150d
yabligowski <student.bl@youngsvilleacademy.org>
2026-04-27 13:01:22 -0400

    Create headerinjectio.js

---
+++ headerinjectio.js
@@ -0,0 +1,8 @@
+let elem = document.createElement("div");
+elem.innerHTML = "<link rel='stylesheet' href = 'https://yabligowski.github.io/wed-dev/header.css'>";
+document.body.prepend(elem);
+
+fetch("https://yabligowski.github.io/wed-dev/header.css")
+.then((result) => result.text())
+.then((text) => {elem.innerHTML = elem.innerHTML +text;})
+.catch((e) => concole.error(e));
