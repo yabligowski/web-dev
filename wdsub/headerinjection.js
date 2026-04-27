8f5082fa60158d28f27eae487f64d6d1c80bf6ba
yabligowski <student.bl@youngsvilleacademy.org>
2026-04-27 13:01:54 -0400

    Delete wdsub/headerinjection.js

--- wdsub/headerinjection.js
+++
@@ -1,8 +0,0 @@
-let elem = document.createElement("div");
-elem.innerHTML = "<link rel='stylesheet' href = 'https://yabligowski.github.io/wed-dev/header.css'>";
-document.body.prepend(elem);
-
-fetch("https://yabligowski.github.io/wed-dev/header.css")
-.then((result) => result.text())
-.then((text) => {elem.innerHTML = elem.innerHTML +text;})
-.catch((e) => concole.error(e));
