2b31b001eb50734f5315a25f7f950582c005622f
yabligowski <student.bl@youngsvilleacademy.org>
2026-05-06 12:48:54 -0400

    Update headerinjection.js

--- headerinjection.js
+++ headerinjection.js
@@ -1,8 +1,8 @@
 let elem = document.createElement("div");
-elem.innerHTML = "<link rel='stylesheet' href = 'https://yabligowski.github.io/wed-dev/header.css'>";
+elem.innerHTML = "<link rel='stylesheet' href = 'https://yabligowski.github.io/web-dev/header.css'>";
 document.body.prepend(elem);
 
-fetch("https://yabligowski.github.io/wed-dev/globalheader.css")
+fetch("https://yabligowski.github.io/web-dev/globalheader.css")
 .then((result) => result.text())
 .then((text) => {elem.innerHTML = elem.innerHTML +text;})
 .catch((e) => console.error(e));
