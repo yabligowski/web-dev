2a9c16851779e257062aeb42aa9b6b0fe199c7fd
yabligowski <student.bl@youngsvilleacademy.org>
2026-05-22 13:28:27 -0400

    Create ttt.cs

---
+++ wdsub/ttt.cs
@@ -0,0 +1,52 @@
+body{
+  margin:0px;
+  background-color:#052C63;
+  }
+
+h1 {
+  color:#5192ED;
+  text-align:center;
+  border: solid #5192ED 3px;
+}
+
+.ttt-row{
+  display:flex;
+  flex:1 1 auto;
+  justify-content:space-evenly;
+  min-width:400px;
+  margin:0px auto;
+  color:white;
+  font-size:100%;
+}
+
+.ttt-square{
+  flex-grow:1;
+  border: solid #5192ED 3px;
+  min-height:100px;
+  color:white;
+  font-size:100%;
+}
+
+#game-over{
+  display:none;
+  background-color:rgba(0,0,5,0.5);
+  position:fixed;
+  top:0px;
+  left:0px;
+  right:0px;
+  bottom:0px;
+  /*width:100vw;
+  height:100hw;*/
+}
+
+#game-over div{
+  background-color:white;
+  width:40%;
+  height:200px;
+  border-radius:10px;
+  text-align:center;
+  padding:30px;
+  margin:200px auto;
+  font-size:2rem;
+}
+
